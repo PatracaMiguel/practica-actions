d768744ab19a83ab8e1b62dbdec8685675224da3
Miguel <miguelangelpatracalagunes@gmail.com>
2026-02-26 22:45:34 -0600

    Correccion de pruebas totales

--- CalculadoraAcciones/src/CalculadoraTest.java
+++ CalculadoraAcciones/src/CalculadoraTest.java
@@ -2,7 +2,7 @@ public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         int pruebasPasadas = 0;
-        int pruebasTotales = 4;
+        int pruebasTotales = 8;
         
         if (calc.sumar(5, 3) == 8) pruebasPasadas++;
         if (calc.sumar(-2, 7) == 5) pruebasPasadas++;
@@ -10,6 +10,7 @@ public class CalculadoraTest {
         if (calc.restar(5, -3) == 8) pruebasPasadas++;
         if (calc.multiplicar(2, 3) == 6) pruebasPasadas++;
         if (calc.multiplicar(5, -2) == 10) pruebasPasadas++;
+
         
         if (pruebasPasadas == pruebasTotales) {
             System.exit(0);
